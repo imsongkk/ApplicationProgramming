c6ccf6b8af566d499d901129042fdbc418dda1d1
Imsung <isyou7487@naver.com>
2022-10-26 02:34:44 +0900

    조작법 추가

--- Assets/Midterm/Scripts/MainScene.cs
+++ Assets/Midterm/Scripts/MainScene.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainScene : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI highscore;
+    [SerializeField] Image tutrorialImage;
 
     // Start is called before the first frame update
     void Start()
     {
         GameManager.Init();
         highscore.text = $"High Score : {GameManager.Instance.highscore}";
+    }
+
+    public void OnClickTutorial()
+    {
+        tutrorialImage.gameObject.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        tutrorialImage.gameObject.SetActive(false);
     }
 
     public void PlayGame()
